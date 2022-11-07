ddd7a1151ed453b732aa007f269778c0fe17b5bb
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-07 22:17:18 +0600

    Update task13.cs

--- task13.cs
+++ task13.cs
@@ -1,1 +1,18 @@
+Console.Write("Введите число : ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int n = Number;
+int Count = 0;
+if (Number<100) Console.WriteLine("Третьего числа нет");
 
+else
+{
+while (n != 0) 
+{
+    n /= 10;
+    Count++;
+}
+
+double Digit =(Number%(Math.Pow(10,Count-2)))/(Math.Pow(10,Count-3));
+int ThirdDigit = (int)Digit;
+Console.WriteLine(ThirdDigit);
+}
