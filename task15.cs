5133f855254f24ffd94f615f126cd0a201071a4c
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-07 22:20:23 +0600

    Update task15.cs

--- task15.cs
+++ task15.cs
@@ -1,1 +1,14 @@
+Console.Write("Введи цифру дня недели: ");
+int DayNumber = Convert.ToInt32(Console.ReadLine());
 
+void CheckTheDay (int DayNumber) {
+  if (DayNumber == 6 || DayNumber == 7) {
+  Console.WriteLine("этот день выходной");
+  }
+  else if (DayNumber < 1 || DayNumber > 7) {
+    Console.WriteLine("эта цифра не подходит");
+  }
+  else Console.WriteLine("это будний день");
+}
+
+CheckTheDay(DayNumber);
